82a3355389bdb5d1fcdcda865af8fc0cb4ab7c95
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-09-18 19:10:18 -0400

    Added MealTableViewCell

---
+++ FetchiOSCodingChallenge/Cells/MealTableViewCell.swift
@@ -0,0 +1,84 @@
+//
+//  MealTableViewCell.swift
+//  FetchiOSCodingChallenge
+//
+//  Created by Space Wizard on 9/18/24.
+//
+
+import Foundation
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    static var identifier: String {
+        String(describing: MealTableViewCell.self)
+    }
+    
+    private var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private var thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(aggregatedMeal: AggregatedMeal) {
+        nameLabel.text = aggregatedMeal.title
+        descriptionLabel.text = aggregatedMeal.description
+        
+        if let image = aggregatedMeal.thumbnailImage {
+            thumbnailImageView.image = image
+        } else {
+            // MARK: TODO Use placeholder image
+            thumbnailImageView.image = nil
+        }
+    }
+}
+
+private extension MealTableViewCell {
+    
+    func setup() {
+        contentView.addSubview(thumbnailImageView)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            thumbnailImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            thumbnailImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            thumbnailImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            thumbnailImageView.widthAnchor.constraint(equalToConstant: 44),
+            thumbnailImageView.heightAnchor.constraint(equalToConstant: 44),
+            
+            nameLabel.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 10),
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            nameLabel.heightAnchor.constraint(equalToConstant: 22),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10),
+            descriptionLabel.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 10),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            descriptionLabel.heightAnchor.constraint(equalToConstant: 22)
+        ])
+    }
+}
