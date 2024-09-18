eacf4303c0ca32a40fc0d80ff04742b79b12af21
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-09-18 18:37:31 -0400

    Testing invoking network service code and successfully decoding results

--- FetchiOSCodingChallenge/View Controllers/ViewController.swift
+++ FetchiOSCodingChallenge/View Controllers/ViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MealsViewController: UIViewController {
+    
+    private let networkService: NetworkProtocol
+    
+    enum Constants {
+        static let urlString: String = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
+    }
+    
+    init(networkService: NetworkProtocol) {
+        self.networkService = networkService
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Task {
+            do {
+                if let meals: AllMeals = try await networkService.executeRequest(urlString: Constants.urlString) {
+                    print(meals)
+                } else {
+                    print("No meals found")
+                }
+                
+            }
+            catch {
+                print(error)
+            }
+        }
     }
 }
 
