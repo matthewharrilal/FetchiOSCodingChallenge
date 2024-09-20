e3aac44527f5abe354f05ef0b9948c78229a4a88
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-09-19 21:37:42 -0400

    Modeling measurements in meal detail object

--- FetchiOSCodingChallenge/Models/MealDetails.swift
+++ FetchiOSCodingChallenge/Models/MealDetails.swift
@@ -17,6 +17,7 @@ struct MealDetail: Decodable {
     let strCategory: String
     let strArea: String
     let strInstructions: String
+
     let strIngredient1: String
     let strIngredient2: String
     let strIngredient3: String
@@ -28,4 +29,18 @@ struct MealDetail: Decodable {
     let strIngredient9: String
     let strIngredient10: String
     let strIngredient11: String
+    
+    let strMeasure1: String
+    let strMeasure2: String
+    let strMeasure3: String
+    let strMeasure4: String
+    let strMeasure5: String
+    let strMeasure6: String
+    let strMeasure7: String
+    let strMeasure8: String
+    let strMeasure9: String
+    let strMeasure10: String
+    let strMeasure11: String
+    let strMeasure12: String
+    let strMeasure13: String
 }
